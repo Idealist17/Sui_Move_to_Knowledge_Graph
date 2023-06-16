c8fe040543c6c013f1e51dc28bbbae7b852026ba
yule <1030845689@qq.com>
2023-06-16 17:49:14 +0800

    add detect8

---
+++ src/detect/detect8.rs
@@ -0,0 +1,107 @@
+// Unnecessary Bool Judgment
+
+use std::{cmp, rc::Rc};
+
+use crate::move_ir::generate_bytecode::FunctionInfo;
+use move_model::symbol::SymbolPool;
+use move_stackless_bytecode::stackless_bytecode::{
+    Bytecode, Operation, Constant
+};
+use move_model::{ty::{PrimitiveType, Type, TypeDisplayContext}};
+
+pub fn detect_unnecessary_bool_judgment(function: &FunctionInfo, local_types: &Vec<Type>) -> bool {
+    let mut ret_flag = false;
+    for (code_offset, bytecode) in function.code.iter().enumerate() {
+        match &bytecode {
+            Bytecode::Call(_, _, Operation::Eq, srcs, _) 
+            | Bytecode::Call(_, _, Operation::Neq, srcs, _) => {
+                let oprand1 = get_oprand_bytecode(&function.code, code_offset-1, srcs[0]);
+                let oprand2 = get_oprand_bytecode(&function.code, code_offset, srcs[1]);
+                if (is_ldbool(oprand1) && ret_is_bool(oprand2, local_types)) || (is_ldbool(oprand2) && ret_is_bool(oprand1, local_types)) {
+                    ret_flag = true;
+                    println!("{:?}", oprand1);
+                    break;
+                }
+            },
+            _ => {
+                continue;
+            }
+        }
+    }
+    ret_flag 
+}
+
+fn get_oprand_bytecode(bytecodes: &Vec<Bytecode>, code_offset: usize, src_idx: usize) -> &Bytecode {
+    let mut tmp_index = code_offset - 1;
+    while tmp_index!=0 {
+        match &bytecodes[tmp_index] {
+            Bytecode::Call(_, dst, _, _, _) => {
+                if dst[0] == src_idx  {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            Bytecode::Assign(_, dst, _, _) => {
+                if *dst == src_idx {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            Bytecode::Load(_, dst, _) => {
+                if *dst == src_idx {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            _ => {
+                tmp_index = tmp_index - 1;
+                continue;
+            }
+        }
+    }
+    return &bytecodes[tmp_index];
+}
+
+fn is_ldbool(bytecode: &Bytecode) -> bool {
+    let mut ret_flag = false;
+    match bytecode {
+        Bytecode::Load(_, _, c) => {
+            if *c == Constant::Bool(true) || *c == Constant::Bool(false){
+                ret_flag = true;
+            }
+        },
+        _ => {
+        }
+    }
+    return ret_flag;
+}
+
+fn ret_is_bool(bytecode: &Bytecode, local_types: &Vec<Type>) -> bool {
+    let mut ret_flag = false;
+    match bytecode {
+        Bytecode::Call(_, dst, _, _, _) => {
+            if local_types[dst[0]] == move_model::ty::Type::Primitive(PrimitiveType::Bool) {
+                ret_flag = true;
+            }
+        },
+        Bytecode::Assign(_, dst, _, _) => {
+            if local_types[*dst] == move_model::ty::Type::Primitive(PrimitiveType::Bool) {
+                ret_flag = true;
+            }
+        },
+        Bytecode::Load(_, dst, _) => {
+            if local_types[*dst] == move_model::ty::Type::Primitive(PrimitiveType::Bool) {
+                ret_flag = true;
+            }
+        },
+        _ => {
+        }
+    }
+    return ret_flag;
+}
