ed2ea184d07d77ff7a5bbace88d3d24e9684ab2c
yule <1030845689@qq.com>
2023-06-18 19:17:23 +0800

    add detect2

--- src/detect/detect2.rs
+++ src/detect/detect2.rs
@@ -1,1 +1,346 @@
 // overflow
+
+use crate::move_ir::generate_bytecode::FunctionInfo;
+use move_binary_format::{views::FunctionDefinitionView, CompiledModule, access::ModuleAccess, file_format::FunctionDefinitionIndex};
+use move_stackless_bytecode::stackless_bytecode::{
+    Bytecode, Operation, Constant
+};
+use ethnum::U256;
+use move_model::{ty::{PrimitiveType, Type}};
+
+pub fn detect_overflow(function: &FunctionInfo, local_types: &Vec<Type>) -> bool {
+    let mut ret_flag = false;
+    for (code_offset, bytecode) in function.code.iter().enumerate() {
+        match &bytecode {
+            Bytecode::Call(_, _ , Operation::Shl, srcs, _) => {
+                let oprand1 = get_oprand_bytecode(&function.code, code_offset-1, srcs[0]);
+                let oprand2 = get_oprand_bytecode(&function.code, code_offset, srcs[1]);
+                if is_ldconst(oprand2) {
+                    let shl_bit = get_const_u8(oprand2).unwrap();
+                    if is_ldconst(oprand1) {
+                        match oprand1 {
+                            Bytecode::Load(_, _, Constant::U8(c)) => {
+                                let num = *c;
+                                if num.leading_zeros() < (shl_bit as u32) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Load(_, _, Constant::U16(c)) => {
+                                let num = *c;
+                                if num.leading_zeros() < (shl_bit as u32) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Load(_, _, Constant::U32(c)) => {
+                                let num = *c;
+                                if num.leading_zeros() < (shl_bit as u32) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Load(_, _, Constant::U64(c)) => {
+                                let num = *c;
+                                if num.leading_zeros() < (shl_bit as u32) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Load(_, _, Constant::U128(c)) => {
+                                let num = *c;
+                                if num.leading_zeros() < (shl_bit as u32) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Load(_, _, Constant::U256(c)) => {
+                                let num = *c;
+                                if num.leading_zeros() < (shl_bit as u32) {
+                                    return true;
+                                }
+                            },
+                            _ => {
+                            }
+                        }
+                    } else if is_assign(oprand1) {
+                        return true;
+                    } else if is_call(oprand1) {
+                        match &oprand1 {
+                            Bytecode::Call(_, _, Operation::Mod, srcs, _) => {
+                                let mod_num = get_oprand_bytecode(&function.code, code_offset, srcs[1]);
+                                if is_ldconst(mod_num) {
+                                    let modnum = get_const(mod_num).unwrap();
+                                    if modnum.leading_zeros() < (shl_bit as u32) {
+                                        return true;
+                                    }
+                                } else {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Call(_, _, Operation::CastU8, src, _) => {
+                                if 8 <= get_ubits(src[0], local_types) + (shl_bit as u16) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Call(_, _, Operation::CastU16, src, _) => {
+                                if 16 <= get_ubits(src[0], local_types) + (shl_bit as u16) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Call(_, _, Operation::CastU32, src, _) => {
+                                if 32 <= get_ubits(src[0], local_types) + (shl_bit as u16) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Call(_, _, Operation::CastU64, src, _) => {
+                                if 64 <= get_ubits(src[0], local_types) + (shl_bit as u16) {
+                                    return true;
+                                }
+                            },
+                            Bytecode::Call(_, _, Operation::CastU128, src, _) => {
+                                if 128 <= get_ubits(src[0], local_types) + (shl_bit as u16) {
+                                    return true;
+                                }
+                            }, 
+                            Bytecode::Call(_, _, Operation::CastU256, src, _) => {
+                                if 256 <= get_ubits(src[0], local_types) + (shl_bit as u16) {
+                                    return true;
+                                }
+                            },
+                            _ => {
+                                continue;
+                            }
+                        }
+                    }
+                } else {
+                    return true;
+                }
+            },
+            _ => {
+                continue;
+            }
+        }
+    }
+    ret_flag
+}
+
+fn get_oprand_bytecode(bytecodes: &Vec<Bytecode>, code_offset: usize, src_idx: usize) -> &Bytecode {
+    let mut tmp_index = code_offset - 1;
+    while tmp_index!=0 {
+        match &bytecodes[tmp_index] {
+            Bytecode::Call(_, dst, _, _, _) => {
+                if dst[0] == src_idx  {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            Bytecode::Assign(_, dst, src, _) => {
+                if *dst == src_idx {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            Bytecode::Load(_, dst, _) => {
+                if *dst == src_idx {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            _ => {
+                tmp_index = tmp_index - 1;
+                continue;
+            }
+        }
+    }
+    return &bytecodes[tmp_index];
+}
+
+fn is_ldconst(bytecode: &Bytecode) -> bool {
+    match bytecode {
+        Bytecode::Load(_, _, _) => {
+            return  true;
+        },
+        _ => {
+            return false;
+        }
+    }
+}
+
+fn is_assign(bytecode: &Bytecode) -> bool {
+    match bytecode {
+        Bytecode::Assign(_, _, _, _)=> {
+            return  true;
+        },
+        _ => {
+            return false;
+        }
+    }
+}
+
+fn is_call(bytecode: &Bytecode) -> bool {
+    match bytecode {
+        Bytecode::Call(_, _, _, _, _)=> {
+            return  true;
+        },
+        _ => {
+            return false;
+        }
+    }
+}
+
+fn get_const(bytecode: &Bytecode) -> Option<U256> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U8(c)) => {
+            return Some(ethnum::U256::from(*c));
+        },
+        Bytecode::Load(_, _, Constant::U16(c)) => {
+            return Some(ethnum::U256::from(*c));
+        },
+        Bytecode::Load(_, _, Constant::U32(c)) => {
+            return Some(ethnum::U256::from(*c));
+        },
+        Bytecode::Load(_, _, Constant::U64(c)) => {
+            return Some(ethnum::U256::from(*c));
+        },
+        Bytecode::Load(_, _, Constant::U128(c)) => {
+            return Some(ethnum::U256::from(*c));
+        },
+        Bytecode::Load(_, _, Constant::U256(c)) => {
+            return Some(ethnum::U256::from(*c));
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn get_const_u8(bytecode: &Bytecode) -> Option<u8> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U8(c)) => {
+            return Some(*c);
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn get_const_u16(bytecode: &Bytecode) -> Option<u16> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U16(c)) => {
+            return Some(*c);
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn get_const_u32(bytecode: &Bytecode) -> Option<u32> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U32(c)) => {
+            return Some(*c);
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn get_const_u64(bytecode: &Bytecode) -> Option<u64> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U64(c)) => {
+            return Some(*c);
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn get_const_u128(bytecode: &Bytecode) -> Option<u128> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U128(c)) => {
+            return Some(*c);
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn get_const_u256(bytecode: &Bytecode) -> Option<U256> {
+    match bytecode {
+        Bytecode::Load(_, _, Constant::U256(c)) => {
+            return Some(*c);
+        },
+        _ => {
+            return Option::None;
+        }
+    }
+}
+
+fn pass_assign(bytecodes: &Vec<Bytecode>, code_offset: usize, src_idx: usize) -> &Bytecode {
+    let mut tmp_index = code_offset - 1;
+    while tmp_index!=0 {
+        match &bytecodes[tmp_index] {
+            Bytecode::Call(_, dst, _, _, _) => {
+                if dst[0] == src_idx  {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            Bytecode::Assign(_, dst, _, _) => {
+                if *dst == src_idx {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            Bytecode::Load(_, dst, _) => {
+                if *dst == src_idx {
+                    return &bytecodes[tmp_index];
+                } else {
+                    tmp_index = tmp_index - 1;
+                    continue;
+                }
+            },
+            _ => {
+                tmp_index = tmp_index - 1;
+                continue;
+            }
+        }
+    }
+    return &bytecodes[tmp_index];
+}
+
+fn get_ubits(src: usize, local_types: &Vec<Type>) -> u16 {
+    match local_types[src] {
+        move_model::ty::Type::Primitive(PrimitiveType::U8) => {
+            return 8;
+        },
+        move_model::ty::Type::Primitive(PrimitiveType::U16) => {
+            return 16;
+        },
+        move_model::ty::Type::Primitive(PrimitiveType::U32) => {
+            return 32;
+        },
+        move_model::ty::Type::Primitive(PrimitiveType::U64) => {
+            return 64;
+        },
+        move_model::ty::Type::Primitive(PrimitiveType::U128) => {
+            return 128;
+        },
+        move_model::ty::Type::Primitive(PrimitiveType::U256) => {
+            return 256;
+        },
+        _ => {
+            return 0;
+        }
+    }
+}
--- src/unit_tests/test_detect.rs
+++ src/unit_tests/test_detect.rs
@@ -2,28 +2,94 @@ use std::path::PathBuf;
 use std::str::FromStr;
 use move_binary_format::access::ModuleAccess;
 use move_binary_format::file_format::FunctionDefinitionIndex;
+use move_binary_format::views::FunctionDefinitionView;
 
+use crate::detect::detect2::detect_overflow;
+use crate::detect::detect3::detect_precision_loss;
+use crate::detect::detect7::detect_unnecessary_type_conversion;
+use crate::detect::detect8::detect_unnecessary_bool_judgment;
 use crate::move_ir::{
     bytecode_display, 
     generate_bytecode::StacklessBytecodeGenerator
 };
 use crate::utils::utils::compile_module;
-use crate::detect::{
-    detect1::detect_unchecked_return,
-    detect5::detect_unused_constants,
-};
-
+use crate::detect::detect1::detect_unchecked_return;
 
 
 #[test]
 fn test_detect_unchecked_return() {
-    let filename = PathBuf::from_str("testdata/examples_mv/aptos/unchecked_return.mv").unwrap();
+    let filename = PathBuf::from_str("/home/yule/Movebit/detect/build/movebit/bytecode_modules/unchecked_return.mv").unwrap();
     let cm = compile_module(filename);
     let mut stbgr = StacklessBytecodeGenerator::new(&cm);
     stbgr.generate_function();
-    for function in stbgr.functions.iter() {
+    for (idx, function) in stbgr.functions.iter().enumerate() {
         if detect_unchecked_return(function) {
-            println!("{}", "111");
+            let name = cm.identifier_at(cm.function_handle_at(cm.function_defs[idx].function).name);
+            println!("{} : {}", name, "unchecked return");
+        }
+    }
+}
+
+#[test]
+fn test_detect_precision_loss() {
+    let filename = PathBuf::from_str("/home/yule/Movebit/detect/build/movebit/bytecode_modules/precision.mv").unwrap();
+    let cm = compile_module(filename);
+    let mut stbgr = StacklessBytecodeGenerator::new(&cm);
+    stbgr.generate_function();
+    for (idx, function) in stbgr.functions.iter().enumerate() {
+        // println!("{:?}",function.code);
+        if detect_precision_loss(function, &stbgr.symbol_pool) {
+            let name = cm.identifier_at(cm.function_handle_at(cm.function_defs[idx].function).name);
+            println!("{} : {}", name, "precision loss");
+        }
+    }
+}
+
+#[test]
+fn test_detect_unnecessary_type_conversion() {
+    let filename = PathBuf::from_str("/home/yule/Movebit/detect/build/movebit/bytecode_modules/unnecessary_type_conversion.mv").unwrap();
+    let cm = compile_module(filename);
+    let mut stbgr = StacklessBytecodeGenerator::new(&cm);
+    stbgr.generate_function();
+    for (idx, function) in stbgr.functions.iter().enumerate() {
+        // println!("{:?}", function.code);
+        // println!("{:?}", &function.local_types);
+        if detect_unnecessary_type_conversion(function, &function.local_types) {
+            let name = cm.identifier_at(cm.function_handle_at(cm.function_defs[idx].function).name);
+            println!("{} : {}", name, "unnecessary type conversion");
+        }
+    }
+}
+
+#[test]
+fn test_detect_unnecessary_bool_judgment() {
+    let filename = PathBuf::from_str("/home/yule/Movebit/detect/build/movebit/bytecode_modules/unnecessary_bool_judgment.mv").unwrap();
+    let cm = compile_module(filename);
+    let mut stbgr = StacklessBytecodeGenerator::new(&cm);
+    stbgr.generate_function();
+    for (idx, function) in stbgr.functions.iter().enumerate() {
+        // println!("{:?}", function.code);
+        if detect_unnecessary_bool_judgment(function, &function.local_types) {
+            let name = cm.identifier_at(cm.function_handle_at(cm.function_defs[idx].function).name);
+            println!("{} : {}", name, "unnecessary bool judgment");
+        }
+    }
+}
+
+#[test]
+fn test_detect_overflow() {
+    let filename = PathBuf::from_str("/home/yule/Movebit/detect/build/movebit/bytecode_modules/overflow.mv").unwrap();
+    let cm = compile_module(filename);
+    let mut stbgr = StacklessBytecodeGenerator::new(&cm);
+    stbgr.generate_function();
+    for (idx, function) in stbgr.functions.iter().enumerate() {
+        println!("{:?}", function.code);
+        let func_define = cm.function_def_at(FunctionDefinitionIndex::new(idx as u16));
+        let view = FunctionDefinitionView::new(&cm, &func_define);
+        let parameters_len = view.parameters().len();
+        if detect_overflow(function, &function.local_types) {
+            let name = cm.identifier_at(cm.function_handle_at(cm.function_defs[idx].function).name);
+            println!("{} : {}", name, "overflow");
         }
     }
 }
